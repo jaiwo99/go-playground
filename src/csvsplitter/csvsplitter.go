7b6c2ab917a2967acc4ad16233053c7ad59a2b69
Liang Shi <liang.shi@asideas.de>
2016-06-03 13:22:49 +0200

    init commit

---
+++ src/csvsplitter/csvsplitter.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"strings"
+	"log"
+	"flag"
+)
+
+type Row struct {
+	Text []string
+}
+func main() {
+	file := flag.String("path", "", "file path")
+	flag.Parse()
+
+	if (*file == "") {
+		fmt.Println("file", *file, "not exist")
+
+		os.Exit(1)
+	}
+
+	csv := parse(*file)
+
+	fmt.Println("")
+	for _,element := range csv {
+
+		for _,elem := range element.Text {
+			fmt.Printf("%-20s", elem)
+		}
+		fmt.Println("")
+	}
+	fmt.Println("")
+}
+
+func parse(file string) []Row {
+	var result []Row
+
+	reader, err := os.Open(file)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer reader.Close()
+
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) > 0 {
+			temp := strings.Split(line, ";")
+			result = append(result, Row{temp})
+		}
+	}
+
+	return result
+}
